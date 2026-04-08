a350d6209b10a762af21fe4171de76b8ef4e99e8
Sasindu Deshan <157575264+deshan2@users.noreply.github.com>
2026-04-09 01:09:46 +0530

    Delete pizzadeliveryapp directory

--- pizzadeliveryapp/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO menu_items (name, description, price, image_url) VALUES
-('Margherita', 'Classic margherita pizza with fresh mozzarella and basil.', 12.99, 'https://static.toiimg.com/thumb/56868564.cms?width=1200&height=900'),
-('Pepperoni', 'Classic pepperoni pizza with mozzarella cheese.', 14.99, 'https://www.kingarthurbaking.com/sites/default/files/2020-02/pepperoni-pizza.jpg'),
-('Vegetarian', 'A delicious vegetarian pizza with a variety of fresh vegetables.', 13.99, 'https://www.twopeasandtheirpod.com/wp-content/uploads/2021/03/Veggie-Pizza-4.jpg');
